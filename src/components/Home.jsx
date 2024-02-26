78d92b4b5626bbfe63c1f8751919fdfe6ad78141
pradeepkumar-es <createthefuture121@gmail.com>
2024-02-27 04:58:56 +0530

    initial code

---
+++ src/components/Home.jsx
@@ -0,0 +1,31 @@
+import React from "react";
+import Navabar from "./Navabar";
+import AboutMe from './AboutMe'
+import Projects from "./Projects";
+import MyJourney from "./MyJourney";
+import Contact from "./Contact";
+import pradeepProfile from "../images/pradeep_linkedin_picture-removebg-preview.png"
+import '../css/home.css'
+export default function Home () {
+    return (
+        <>
+        <Navabar/>
+        <div className="introduction">
+            <div className="about">
+                <h3>Web Developer</h3>
+                <h1>Elevating Brands Through Innovative Web Solutions</h1>
+                <p>My Mission is to create Engaging, Fast, and Loving Websites for Clients and User </p>
+                <div className="message-me">Chat Me on Linkein</div>
+            </div>
+            <div className="profile">
+                <img className="pradeep-profile" src={pradeepProfile} alt="Profile of Pradeep" />
+            </div>
+        </div>
+        <AboutMe/>
+        <Projects/>
+        <MyJourney/>
+        <Contact/>
+        <Navabar/>
+        </>
+    )
+}
---
+++ src/components/MyJourney.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function MyJourney() {
+  return (
+    <div>
+      My Journey
+    </div>
+  )
+}
+
+export default MyJourney
