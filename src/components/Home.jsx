0f798e4ea5ad0e670b97507f99b0087f7d026013
pradeepkumar-es <createthefuture121@gmail.com>
2024-02-27 22:57:46 +0530

    contact

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -9,10 +9,15 @@ import webIcon from "../images/web-icon.png"
 import linkedinIcon from "../images/linkedin-icon.png"
 import mailIcon from "../images/mail-icon.png"
 import '../css/home.css'
+import { FaLinkedin } from "react-icons/fa";
+import { FaGlobe } from "react-icons/fa";
+import { IoMdMail } from "react-icons/io";
+import { IoIosArrowRoundForward } from "react-icons/io";
 export default function Home() {
     return (
         <>
             <Navabar />
+            <div className="home">
             <div className="introduction">
                 <div className="about">
                     <h3>WEB DEVELOPER & DESIGNER</h3>
@@ -21,9 +26,13 @@ export default function Home() {
                     <div className="message-me">
                         <p>Contact Me</p>
                         <div className="social-icons">
-                            <img className="web" src={webIcon} alt="contact through website" />
-                            <img className="linkedin" src={linkedinIcon} alt="contact through linkedin" />
-                            <img className="email" src={mailIcon} alt="contact through Email" />
+                            {/* <img className="web" src={webIcon} alt="contact through website" /> */}
+                            {/* <img className="linkedin" src={linkedinIcon} alt="contact through linkedin" /> */}
+                            {/* <img className="email" src={mailIcon} alt="contact through Email" /> */}
+                            <IoIosArrowRoundForward className="arrow"/>
+                            <a href="mailto:pradeepkumariitk22@gmail.com"><IoMdMail className="email"/></a>
+                            <a href="https://www.linkedin.com/in/pradeepkumar-es/"><FaLinkedin className="linkedin" /></a>
+                            <a href="#"><FaGlobe className="web"/></a>
                         </div>
                     </div>
                 </div>
@@ -36,6 +45,7 @@ export default function Home() {
             <MyJourney />
             <Contact />
             <Navabar />
+        </div>
         </>
     )
 }
