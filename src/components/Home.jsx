cb906f81b873f48585ac53150261400d971f4659
pradeepkumar-es <createthefuture121@gmail.com>
2024-02-27 06:31:01 +0530

    ui

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,7 +4,7 @@ import AboutMe from './AboutMe'
 import Projects from "./Projects";
 import MyJourney from "./MyJourney";
 import Contact from "./Contact";
-import pradeepProfile from "../images/pradeep_linkedin_picture-removebg-preview.png"
+import pradeepProfile from "../images/pradeep-pic-nobg.png"
 import '../css/home.css'
 export default function Home () {
     return (
@@ -12,7 +12,7 @@ export default function Home () {
         <Navabar/>
         <div className="introduction">
             <div className="about">
-                <h3>Web Developer</h3>
+                <h3>WEB DEVELOPER & DESIGNER</h3>
                 <h1>Elevating Brands Through Innovative Web Solutions</h1>
                 <p>My Mission is to create Engaging, Fast, and Loving Websites for Clients and User </p>
                 <div className="message-me">Chat Me on Linkedin</div>
