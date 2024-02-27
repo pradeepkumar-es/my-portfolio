a69ae32f7684d6139d6404e97dd7f93dda784c05
pradeepkumar-es <createthefuture121@gmail.com>
2024-02-27 07:26:52 +0530

    social link

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,27 +5,37 @@ import Projects from "./Projects";
 import MyJourney from "./MyJourney";
 import Contact from "./Contact";
 import pradeepProfile from "../images/pradeep-pic-nobg.png"
+import webIcon from "../images/web-icon.png"
+import linkedinIcon from "../images/linkedin-icon.png"
+import mailIcon from "../images/mail-icon.png"
 import '../css/home.css'
-export default function Home () {
+export default function Home() {
     return (
         <>
-        <Navabar/>
-        <div className="introduction">
-            <div className="about">
-                <h3>WEB DEVELOPER & DESIGNER</h3>
-                <h1>Elevating Brands Through Innovative Web Solutions</h1>
-                <p>My Mission is to create Engaging, Fast, and Loving Websites for Clients and User </p>
-                <div className="message-me">Chat Me on Linkedin</div>
+            <Navabar />
+            <div className="introduction">
+                <div className="about">
+                    <h3>WEB DEVELOPER & DESIGNER</h3>
+                    <h1>Elevating Brands Through Innovative Web Solutions</h1>
+                    <p>My Mission is to create Engaging, Fast, and Loving Websites for Clients and User </p>
+                    <div className="message-me">
+                        <p>Contact Me</p>
+                        <div className="social-icons">
+                            <img className="web" src={webIcon} alt="contact through website" />
+                            <img className="linkedin" src={linkedinIcon} alt="contact through linkedin" />
+                            <img className="email" src={mailIcon} alt="contact through Email" />
+                        </div>
+                    </div>
+                </div>
+                <div className="profile">
+                    <img className="pradeep-profile" src={pradeepProfile} alt="Profile of Pradeep" />
+                </div>
             </div>
-            <div className="profile">
-                <img className="pradeep-profile" src={pradeepProfile} alt="Profile of Pradeep" />
-            </div>
-        </div>
-        <AboutMe/>
-        <Projects/>
-        <MyJourney/>
-        <Contact/>
-        <Navabar/>
+            <AboutMe />
+            <Projects />
+            <MyJourney />
+            <Contact />
+            <Navabar />
         </>
     )
 }
