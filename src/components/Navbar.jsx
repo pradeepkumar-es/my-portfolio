37425708e45105c1124d34748467cdc0a41196e4
pradeepkumar-es <createthefuture121@gmail.com>
2024-02-29 06:21:43 +0530

    mobileNav

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,31 +1,45 @@
-import React from 'react'
-import { FaInstagram,FaLinkedin,FaFacebook } from "react-icons/fa";
-import {GiHamburgerMenu} from "react-icons/gi"
+import React, { useState } from 'react'
+import { FaInstagram, FaLinkedin, FaFacebook } from "react-icons/fa";
+import { GiHamburgerMenu } from "react-icons/gi"
+import { RxCross1 } from "react-icons/rx"
 import "../css/navbar.css"
-export default function Navabar () {
+export default function Navabar() {
+    const [openMenu, setOpenMenu] = useState(true);
+    function togglemenu() {
+        setOpenMenu(!openMenu);
+    }
     return (
         <>
-        <div className="nav">
-            <div className="logo"><h2><span>P</span>RADEEP <span>K</span>UMAR</h2></div>
-            <div className="menu-link">
-                <ul>
-                    <li>About Me</li>
-                    <li>My Journey</li>
-                    <li>My Projects</li>
-                    <li>Contact Me</li>
-                </ul>
+            <div className="nav">
+                <div className="logo"><h2><span>P</span>RADEEP <span>K</span>UMAR</h2></div>
+                <div className="menu-link">
+                    <ul>
+                        <li>About Me</li>
+                        <li>My Journey</li>
+                        <li>My Projects</li>
+                        <li>Contact Me</li>
+                    </ul>
+                </div>
+                <div className="social-link">
+                    <ul>
+                        <li><a href="https://www.instagram.com/pradeepkumar.es/"><FaInstagram /></a></li>
+                        <li><a href="https://www.linkedin.com/in/pradeepkumar-es/"><FaLinkedin /></a></li>
+                        <li><a href="https://www.facebook.com/pradeepkumar.es/"><FaFacebook /></a></li>
+                    </ul>
+                </div>
+                <div className="mobile-menu">
+                    <GiHamburgerMenu onClick={togglemenu} className={openMenu ? '' : 'deactivate-hamburgerMenu'} />
+                    <div className="showMenuPart">
+                    <div className="cross"><RxCross1 onClick={togglemenu} className={openMenu ? 'activate-cross' : 'crossStyle'} /></div>
+                    <ul className={openMenu ? 'activate-mobileNavbar' : 'mobileMenuStyle'}>
+                        <li>About Me</li>
+                        <li>My Journey</li>
+                        <li>My Projects</li>
+                        <li>Contact Me</li>
+                    </ul>
+                    </div>
+                </div>
             </div>
-            <div className="social-link">
-                <ul>
-                    <li><a href="https://www.instagram.com/pradeepkumar.es/"><FaInstagram/></a></li>
-                    <li><a href="https://www.linkedin.com/in/pradeepkumar-es/"><FaLinkedin/></a></li>
-                    <li><a href="https://www.facebook.com/pradeepkumar.es/"><FaFacebook/></a></li>
-                </ul>
-            </div>
-            <div className="mobile-menu-link">
-                <GiHamburgerMenu className='hamburger-menu'/>
-            </div>
-        </div>
         </>
     )
 }
