5d302b4dd8ac34dcbf7d33c2c5a2453f79f7aac3
pradeepkumar-es <createthefuture121@gmail.com>
2024-06-06 23:57:01 +0530

    Search Filter in project page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,19 +3,39 @@ import ProjectCard from './ProjectCard'
 import { projects } from './data'
 import '../css/projectpage.css'
 function Projects() {
+  //storing the input value in the state
+  const [searchTerm, setSearchTerm] = React.useState('');
+  function handleSearch(event) {
+    setSearchTerm(event.target.value)
+  }
   return (
     <div className="projectpage">
       <h1>My Projects (Prototype Version)</h1>
       <p>*UI/UX and Features of all mini projects of this section will be added & updated time to time.</p>
+      <input
+        type="text"
+        placeholder='Search Project.....'
+        name='search'
+        // value={searchTerm}
+        onChange={handleSearch}
+      />
       <div className="projectlist">
         {
-          projects.map((data) => {
+          // Filter
+          projects.filter((item) => {
+            if (searchTerm == '') {
+              return item;
+            } else if (item.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+              return item;
+            }
+          }
+          ) /*mapping to filtered array */
+          .map((data) => {
             return (
               <>
-
                 <ProjectCard
                   primage={data.image}
-                  prtech ={data.tech}
+                  prtech={data.tech}
                   prname={data.name}
                   prlink={data.link}
                   prdetail={data.detail}
